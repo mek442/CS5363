a9245d58f594e23daeaf3893d6e32c4c0cc0dc23
mek442 <mek442@my.utsa.edu>
2013-10-01 15:26:39 -0500

    - Added keyword and other token word as Enum

--- core/src/com/mek442/scanner/Token.java
+++ core/src/com/mek442/scanner/Token.java
@@ -1,8 +1,39 @@
 package com.mek442.scanner;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author Shaikh Mostafa<mek442@my.utsa.edu>
  * 
  */
-public class Token {
+enum Token {
+	BOOL("bool"), INT("int"), IF("if"), THEN("then"), ELSE("else"), BEGIN("begin"), END("end"), WHILE("while"), DO("do"), PROGRAM(
+			"program"), VAR("var"), AS("as"), LP("("), RP(")"), ASGN(":="), SC(";"), MUL("*"), DIV("div"), MOD("mod"), PLUS(
+			"+"), MINUS("-"), EQUAL("="), NOT_EQUAL("!="), LT("<"), GT(">"), LT_EQUAL("<="), GT_EQUAL(">="),
+			WRITEINT("writeInt"),READINT("readInt");
+
+	private String name;
+
+	Token(String pName) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return this.name;
+	}
+
+	public List<Token> getOP2() {
+		return Collections.unmodifiableList(Arrays.asList(MUL, MOD, DIV));
+	}
+
+	public List<Token> getOP3() {
+		return Collections.unmodifiableList(Arrays.asList(PLUS, MINUS));
+	}
+
+	public List<Token> getOP4() {
+		return Collections.unmodifiableList(Arrays.asList(EQUAL, NOT_EQUAL, LT, LT_EQUAL, GT, GT_EQUAL));
+	}
 
 }
